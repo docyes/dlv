015f1a081f3a78864e91fc014fc411c9b7b2d52b
docyes <carl@featureblend.com>
2013-12-04 22:49:35 -0800

    initial stab at general listenTo/stopListening declarative hash for observables

---
+++ backvent.js
@@ -0,0 +1,43 @@
+var BackVent = Backbone.View.extend({
+    constructor: function(options) {
+        options || (options = {});
+        if (options.listeners) {
+            this.listeners = options.listeners;
+        }
+        Backbone.View.apply(this, arguments);
+        this.delegateListeners();
+    },
+    delegateListeners: function(listeners) {
+        if (!(listeners || (listeners = _.result(this, 'listeners')))) {
+            return this;
+        }
+        this.undelegateListeners();
+        for (var key in listeners) {
+            var method = listeners[key];
+            if (!_.isFunction(method)) {
+                method = this[listeners[key]];
+            }
+            if (!method) {
+                continue;
+            }
+            var match = key.match(/\S+/g);
+            var other = match.pop();
+            other = (other === 'this') ? this : this[other];
+            if (!other) {
+                continue;
+            }
+            this.listenTo(other, match.join(' '), method);
+        }
+    },
+    undelegateListeners: function() {
+        for (var key in listeners) {
+            var match = key.match(/\S+/g);
+            var other = match.pop();
+            other = (other === 'this') ? this : this[other];
+            if (!other) {
+                continue;
+            }
+            this.stopListening(other);
+        }
+    }
+});
