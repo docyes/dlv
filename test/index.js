4e3d017aaa59222864a5268886d1b7d92c2321d0
docyes <carl@featureblend.com>
2013-12-06 22:57:08 -0800

    add constructor tests

--- test/index.js
+++ test/index.js
@@ -5,7 +5,15 @@
         teardown: function() {
         }
     });
-    test('initialize', 0, function() {
-    
+    test('initialize with nothing', 1, function() {
+        var dlv = new DLV();
+        ok(!dlv.listeners, 'no constructor instance listeners');    
+    });
+    test('initialize with listeners', 1, function() {
+        var listeners = {};
+        var dlv = new DLV({
+               listeners: listeners
+            });
+        strictEqual(dlv.listeners, listeners, 'constructor instance listeners');    
     });
 })();
