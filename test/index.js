7e8d2b51b078156183165036d6043ff33d8c4db6
docyes <carl@featureblend.com>
2013-12-06 21:35:42 -0800

    test stubs

---
+++ test/index.js
@@ -0,0 +1,11 @@
+(function() {
+    module('DLV', {
+        setup: function() {
+        },
+        teardown: function() {
+        }
+    });
+    test('initialize', 0, function() {
+    
+    });
+})();
